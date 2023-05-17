28241a43d051166ace88fa42ed4e5a261a04f844
art antonio <artantonio.it@gmail.com>
2023-05-17 08:24:04 +0800

    setting up environment

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+
+const app = express();
+
+//register view engine
+
+app.set('view engine', 'ejs');
+app.set(express.static('public'));
+app.listen(3000);
+//morgan
+// app.use(morgan('dev'))
+// middleware static files
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.render('index', { title: 'Home' });
+});
+app.get('/about', (req, res) => {
+	res.render('about', { title: 'About' });
+});
+app.get('/contact', (req, res) => {
+	res.render('contact', { title: 'Contact' });
+});
+
+// 404 page
+app.use((req, res) => {
+	// res.status(404).sendFile('./views/404.html', { root : __dirname })
+	res.status(404).render('404', { title: '404' });
+});
